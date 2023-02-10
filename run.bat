7f016642a111e8091466dafa607696bd14bfc23a
Frank Averhoff <faverhofff@gmail.com>
2023-02-10 10:23:17 +0000

    Fixed location into run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 docker run -d -p 27017:27017 --name test-mongo mongo:latest
 
-cd src\DoctorAppoinmentApi
+cd src
 docker build -t doctor_appointment -f Dockerfile .
 docker run doctor_appointment
