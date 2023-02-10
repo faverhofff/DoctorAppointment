9f6272561820d65fbe9731add6bdc9aedab126f6
Frank Averhoff <faverhofff@gmail.com>
2023-02-10 09:26:26 +0000

    Added mongodb container to the batch file

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
+docker run -d -p 27017:27017 --name test-mongo mongo:latest
+
 cd src\DoctorAppoinmentApi
 docker build -t doctor_appointment -f Dockerfile .
 docker run doctor_appointment
