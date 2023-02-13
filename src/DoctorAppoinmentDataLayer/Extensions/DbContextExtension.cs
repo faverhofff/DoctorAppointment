fab510e86c09ab62f193b907fc868b9b832f10ba
Frank Averhoff <faverhofff@gmail.com>
2023-02-13 08:22:31 +0000

    Updated data layer project

---
+++ src/DoctorAppoinmentDataLayer/Extensions/DbContextExtension.cs
@@ -0,0 +1,41 @@
+﻿using DoctorAppointmentDataLayer;
+using DoctorAppointmentDataLayer.Models;
+using MongoDB.Driver;
+using ParkBee.MongoDb;
+using System;
+using System.Linq.Expressions;
+
+namespace DoctorAppointmentDataLayer.Extensions
+{
+    public static class DbContextExtension
+    {
+        #region issue mongodb context
+        public static FilterDefinition<TEntity> GetFindByKeyFilterDefinition<TEntity>(this MongoDbContext dbContext, object keyValue) where TEntity : class
+        {
+            var memberExpression = GetFilterByKeyExpression<TEntity>(dbContext.Database);
+            var parameterExpression = Expression.Parameter(typeof(TEntity), "e");
+            var left = Expression.Property(parameterExpression, memberExpression.Member.Name);
+            var right = Expression.Constant(keyValue, keyValue.GetType());
+
+            var predicate = Expression.Lambda<Func<TEntity, bool>>(
+                Expression.Equal(left, right), parameterExpression
+            );
+            return new ExpressionFilterDefinition<TEntity>(predicate);
+        }
+
+        private static MemberExpression GetFilterByKeyExpression<TEntity>(IMongoDatabase database) where TEntity : class 
+        {
+            var entity = new EntityTypeBuilder<TEntity>(database);
+            entity.HasKey(p => (p as BaseModel).Id);
+            var builder = entity as EntityTypeBuilder<TEntity>;
+
+            if (builder.KeyPropertyExpression == null)
+            {
+                throw new InvalidOperationException("HasKey invalid");
+            }
+
+            return ((builder.KeyPropertyExpression as LambdaExpression).Body as Expression) as MemberExpression;
+        }
+        #endregion 
+    }
+}
--- src/DoctorAppoinmentDataLayer/MongoDbContext.cs
+++ src/DoctorAppoinmentDataLayer/MongoDbContext.cs
@@ -6,27 +6,24 @@ namespace DoctorAppointmentDataLayer
 {
     public class MongoDbContext : MongoContext
     {
+        private readonly IMongoContextOptionsBuilder optionsBuilder;
         public MongoDbContext(IMongoContextOptionsBuilder optionsBuilder) : base(optionsBuilder)
         {
+            this.optionsBuilder = optionsBuilder;
         }
 
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Appointment> Appointments { get; set; }
 
+        public IMongoContextOptionsBuilder GetOptionsBuilder() => OptionsBuilder;
+
         protected override void OnConfiguring()
         {
-            OptionsBuilder.Entity<Patient>(async entity => entity.HasKey(p => p.Id));
-            OptionsBuilder.Entity<Doctor>(async entity => entity.HasKey(p => p.Id));
-            OptionsBuilder.Entity<Appointment>(async entity => entity.HasKey(p => p.Id));
+            OptionsBuilder.Entity<Patient>(entity => entity.HasKey(p => p.Id));
+            OptionsBuilder.Entity<Doctor>(entity => entity.HasKey(p => p.Id));
+            OptionsBuilder.Entity<Appointment>(entity => entity.HasKey(p => p.Id));
         }
 
-        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        //{
-        //    // To remove the requests to the Migration History table
-        //    Database.SetInitializer<MongoDbContext>(null);
-        //    // To remove the plural names   
-        //    modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-        //}
     }
 }
